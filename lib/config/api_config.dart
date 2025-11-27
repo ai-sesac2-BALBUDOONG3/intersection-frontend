42b585c38dd97a0642eb330a0a23bc7e12a70265
ckddnjs1173 <ckddnjs1173@gmail.com>
2025-11-27 15:29:34 +0900

    chore: point Flutter frontend to Azure API (azurebackup)

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,9 +1,17 @@
 // lib/config/api_config.dart
 
+/// API 엔드포인트 설정
+///
+/// - 로컬 개발용: http://127.0.0.1:8000
+/// - 프로덕션(Azure App Service):
+///   https://intersection-api-balbudoong-dvaefbfhbychg9dc.canadacentral-01.azurewebsites.net
+///
+/// 지금은 Azure 백엔드에 붙어서 테스트하므로 PROD_URL 을 사용한다.
 class ApiConfig {
-  // 애뮬레이터에서 테스트할 때
-  //static const String baseUrl = 'http://10.0.2.2:8000';
+  // Azure App Service (intersection-api-balbudoong)
+  static const String baseUrl =
+      "https://intersection-api-balbudoong-dvaefbfhbychg9dc.canadacentral-01.azurewebsites.net";
 
-  // 웹에서 테스트하면 아래로 바꾸면 됨
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  // 필요하면 나중에 스위치해서 쓸 수 있게 로컬 URL도 참고용으로 남겨둠
+  static const String localUrl = "http://127.0.0.1:8000";
 }
